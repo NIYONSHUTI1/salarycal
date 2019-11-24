2ba3c4facbdb60ff81c9c981b99a2a0e708ff3e5
NIYINSHUTI1 <niyonshutiaimee9@gmail.com>
2019-11-24 09:07:22 -0800

    first commit

---
+++ salary/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Salary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator cal=new Calculator();
+           
+            cal.em_id = Convert.ToInt32(Console.ReadLine());
+           
+            cal.em_name = Console.ReadLine();
+           
+            cal.em_position = Console.ReadLine();
+           
+            cal.em_gendeer= Console.ReadLine();
+            
+            double grossSalary;
+            grossSalary = Convert.ToDouble(Console.ReadLine());
+            
+            Console.WriteLine($"{cal.em_name} your monthly salary on position of {cal.em_position}   is {cal.MonthlySalary(grossSalary)}");
+            
+            Console.WriteLine($"{cal.em_name} your one-time salary on position of {cal.em_position} is {cal.OneTimeSalary(grossSalary)}");
+                
+            Console.WriteLine("Enter your rate ");
+            double rate;
+            rate = Convert.ToDouble(Console.ReadLine());
+            
+            double hour;
+            hour = Convert.ToDouble(Console.ReadLine());
+            if (rate != 0 && hour != 0)
+            {
+                Console.WriteLine($"{cal.em_name} Your hourly salary is {cal.HourlySalary(rate,hour)}");
+            }
+            else
+            {
+                Console.WriteLine(" Your hourly salary is 0");
+            }
+            
+           
+            
+            
+        }
+    }
+}
---
+++ salary/SalaryCal.cs
@@ -0,0 +1,31 @@
+﻿namespace Salary
+{
+    public class Calculator
+    {
+        public int em_id;
+        public string em_name;
+        public string em_position;
+        public string em_gendeer;
+
+        public double MonthlySalary(double grossSalary)
+        {
+            double salary;
+            salary = grossSalary - (((grossSalary * 30) / 100) + ((grossSalary * 5) / 100) + ((grossSalary * 3) / 100));
+            return salary;
+        }
+
+        public double HourlySalary(double rate, double hour)
+        {
+            double salary;
+            salary=(rate*hour)-((((rate*hour)*5)/100)+(((rate*hour)*3)/100));
+            return salary;
+        }
+
+        public double OneTimeSalary(double grossSalary)
+        {
+            double salary;
+            salary = grossSalary - ((grossSalary * 30) / 100);
+            return salary;
+        }
+    }
+}
